e99f0eaba7a620e7c381a91885dc83309f96bbb4
AchrafRe <43996450+AchrafRe@users.noreply.github.com>
2018-10-13 21:21:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,68 +2,23 @@
 //
 #include "stdafx.h"
 #include <iostream>
+#include "Sort.h"
 
 
-int bubbles[6]{ 3,1,9,2,6,4 };
-int swapper;
-bool NoError = false;
+
+Sort Obj;
  
 int main()
 {
+	Obj.PrintNumbers();
 
-	for (int x = 0; x < 6; x++)
-	{
-		std::cout << bubbles[x] << std::endl;
-	}
+	Obj.BubbleSort();
 
+	std::cout << std::endl;
 
-	while (NoError == false)
-	{
-		if (bubbles[1] < bubbles[0])
-		{
-			swapper = bubbles[1];
-			bubbles[1] = bubbles[0];
-			bubbles[0] = swapper;
-	
-		}
-		if (bubbles[2] < bubbles[1])
-		{
-			swapper = bubbles[2];
-			bubbles[2] = bubbles[1];
-			bubbles[1] = swapper;
-		}
-		if (bubbles[3] < bubbles[2])
-		{
-			swapper = bubbles[3];
-			bubbles[3] = bubbles[2];
-			bubbles[2] = swapper;
-		}
-		if (bubbles[4] < bubbles[3])
-		{
-			swapper = bubbles[4];
-			bubbles[4] = bubbles[3];
-			bubbles[3] = swapper;
-		}
-		if (bubbles[5] < bubbles[4])
-		{
-			swapper = bubbles[5];
-			bubbles[5] = bubbles[4];
-			bubbles[4] = swapper;
-		}
-		if (bubbles[0] <= bubbles[1] && bubbles[1] <= bubbles[2] && bubbles[2] <= bubbles[3] && bubbles[3] <= bubbles[4] && bubbles[4] <= bubbles[5])
-		{
-			NoError = true;
-		}
-		
-	}
-
-	std::cout << std::endl << std::endl;
-
-	for (int z = 0; z < 6; z++)
-	{
-		std::cout << bubbles[z] << std::endl;
-	}
+	Obj.PrintNumbers();
 
     return 0;
 }
+
 
